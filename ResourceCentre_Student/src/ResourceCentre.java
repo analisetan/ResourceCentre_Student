860f28568b78b83623fb359a3c61e5b1989cc0e8
19046242 <19046242@DESKTOP-J6TS7B9>
2020-07-21 16:21:40 +0800

    han

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -199,25 +199,27 @@ public class ResourceCentre {
 		
 	}	
 	public static void addChromebook(ArrayList<Chromebook> chromebookList, Chromebook cb) {
-<<<<<<< HEAD
+
 		//fail("Not yet implemented");
 				// Item list is not null, so that can add a new item
 				assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 						
+				Chromebook cc1;
 				//Given an empty list, after adding 1 item, the size of the list is 1
 				ResourceCentre.addChromebook(chromebookList, cc1);		
 				assertEquals("Test if that Chromebook arraylist size is 2?", 2, chromebookList.size());
 						
+				Object cc2;
 				//The item just added is as same as the first item of the list
 				assertSame("Test that Chromebook is added same as 2nd item of the list?", cc2, chromebookList.get(0));
 						
+				Chromebook cc3;
 				//Add another item. test The size of the list is 2?
 				ResourceCentre.addChromebook(chromebookList, cc3);
 				assertEquals("Test that Camcorder arraylist size is 3?", 3, chromebookList.size());
 				
 				//---
 			}
-=======
 		// write your code here
 		String tag = Helper.readString("Enter assest tag >");
 		String desc = Helper.readString("Enter description >");
@@ -225,8 +227,9 @@ public class ResourceCentre {
 		chromebookList.add(new Chromebook (tag, desc, os));
 		
 		System.out.println("Chromebook added");
->>>>>>> branch 'master' of https://github.com/analisetan/ResourceCentre_Student.git
-	}
+
+		}
+		
 	
 	//================================= Option 3 Loan =================================
 	public static boolean doLoanCamcorder(ArrayList<Camcorder> camcorderList, String tag, String dueDate) {
